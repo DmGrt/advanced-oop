c4c1609a09279a22b699e6917b914a41894a47a3
Dm_Grt <grytsishin@gmail.com>
2020-07-29 16:08:06 +0300

    Changed text placement

--- src/main/java/Executor.java
+++ src/main/java/Executor.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Executor {
     public static void main(String[] args) {
         Figure[] figures = generateFigures(5);
-        for (Figure figure : figures){
+        for (Figure figure : figures) {
             System.out.println(figure);
             System.out.println(figure.drawFigure());
         }
@@ -12,19 +12,26 @@ public class Executor {
     public static Figure[] generateFigures(int numberOfFigures) {
         Figure[] response = new Figure[numberOfFigures];
         Random random = new Random();
-        for (int i = 0; i < numberOfFigures; i++){
-            switch (random.nextInt(3)){
+        for (int i = 0; i < numberOfFigures; i++) {
+            switch (random.nextInt(3)) {
                 case 0:
-                    response[i] = new Square(Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(50));
+                    response[i] = new Square(Colors.values()[new Random().nextInt(Colors.values().length)],
+                            new Random().nextInt(50));
                     break;
                 case 1:
-                    response[i] = new Triangle(Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(45),new Random().nextInt(38));
+                    response[i] = new Triangle(Colors.values()[new Random().nextInt(Colors.values().length)],
+                            new Random().nextInt(45),
+                            new Random().nextInt(38));
                     break;
                 case 2:
-                    response[i] = new Circle(Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(50));
+                    response[i] = new Circle(Colors.values()[new Random().nextInt(Colors.values().length)],
+                            new Random().nextInt(50));
                     break;
                 case 3:
-                    response[i] = new Trapeze(Colors.values()[new Random().nextInt(Colors.values().length)], new Random().nextInt(50),new Random().nextInt(45), new Random().nextInt(78));
+                    response[i] = new Trapeze(Colors.values()[new Random().nextInt(Colors.values().length)],
+                            new Random().nextInt(50),
+                            new Random().nextInt(45),
+                            new Random().nextInt(78));
                     break;
             }
         }
